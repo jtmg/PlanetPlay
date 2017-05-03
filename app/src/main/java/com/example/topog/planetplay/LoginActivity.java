75437750cda4c499d33e825ff8a410d5cad1948b
José Tiago Mendonça Gouveia <jose.t.m.gouveia@gmail.com>
2017-05-03 01:54:42 +0100

    login interface done

--- app/src/main/java/com/example/topog/planetplay/LoginActivity.java
+++ app/src/main/java/com/example/topog/planetplay/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.topog.planetplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Create_Account(View view) {
+    }
+
+    public void Login(View view) {
     }
 }
